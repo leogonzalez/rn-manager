ea8e9a5f4b2d3351e7199601ca60995012b3a73c
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-05-25 20:10:07 -0300

    firebase added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import firebase from "firebase";
 import reducers from "./reducers";
+import { config } from "./config/keys";
 
 const store = createStore(reducers);
 
 class App extends Component {
-  state = {};
+  componentWillMount() {
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
-import initialReducer from './test'
 
 export default combineReducers({
-  initial: initialReducer
+  initial: () => []
 })
